d8503b98677cfef36356243c7a6d45f33542d70d
Seba Juarez <sdjuarez@gmail.com>
2024-06-22 01:51:57 +0200

    Initial Commit

---
+++ DemoSum/DemoSumApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSumApp.swift
+//  DemoSum
+//
+//  Created by Sebastian Juarez on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoSumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
